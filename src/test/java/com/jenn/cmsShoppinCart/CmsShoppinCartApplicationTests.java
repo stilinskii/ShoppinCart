91940e58353f23400fec6f2757c00522dfce069a
stilinskii <lhy1031hy@gmail.com>
2022-06-27 22:36:49 +0900

    Initial commit

---
+++ src/test/java/com/jenn/cmsShoppinCart/CmsShoppinCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenn.cmsShoppinCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsShoppinCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
